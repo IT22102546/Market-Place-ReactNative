e887af738587833e0300e7c7af275acc0254f989
Sanjana Nimesh <sanjana.nim2001@gmail.com>
2024-08-30 22:50:51 +0530

    List created

--- Apps/Navigations/ProfileScreenStackNav.jsx
+++ Apps/Navigations/ProfileScreenStackNav.jsx
@@ -1,40 +1,50 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { createStackNavigator } from '@react-navigation/stack'
+import { View, Text } from 'react-native';
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
 import ProfileScreen from '../Screen/ProfileScreen';
 import MyProduct from '../Screen/MyProduct';
 import ProductDetail from '../Screen/ProductDetail';
+import MyList from '../Screen/MyList';  // Import MyList component
 
 export default function ProfileScreenStackNav() {
     const Stack = createStackNavigator();
-  return (
-    <Stack.Navigator>
-        <Stack.Screen name="profile-tab" 
-         options={{
-            headerShown:false
-         }}
-        component={ProfileScreen}/>
-        <Stack.Screen name="my-product" component={MyProduct}
+  
+    return (
+      <Stack.Navigator>
+          <Stack.Screen 
+            name="profile-tab" 
             options={{
-                headerStyle:{
-                    backgroundColor:'#3b82f6'
-                },
-                headerTintColor:'#fff',
-                headerTitle:'My Products' 
+                headerShown: false
             }}
-        />
-
-        <Stack.Screen name='product-detail' component={ProductDetail}
+            component={ProfileScreen}
+          />
+          <Stack.Screen 
+            name="my-product" 
+            component={MyProduct}
             options={{
-                headerStyle:{
-                    backgroundColor:'#3b82f6'
-                },
-                headerTintColor:'#fff',
-                headerTitle:'Detail' 
+                headerStyle: { backgroundColor: '#3b82f6' },
+                headerTintColor: '#fff',
+                headerTitle: 'My Products'
             }}
-            
-        />
-        
-    </Stack.Navigator>
-  )
+          />
+          <Stack.Screen 
+            name="product-detail" 
+            component={ProductDetail}
+            options={{
+                headerStyle: { backgroundColor: '#3b82f6' },
+                headerTintColor: '#fff',
+                headerTitle: 'Detail'
+            }}
+          />
+          <Stack.Screen 
+            name="my-list" 
+            component={MyList} 
+            options={{
+                headerStyle: { backgroundColor: '#3b82f6' },
+                headerTintColor: '#fff',
+                headerTitle: 'My List'
+            }}
+          />
+      </Stack.Navigator>
+    );
 }
